917cbf5412ae05bfdf2711f3968f7ee9a12abca5
ashik112 <md.ashekur@gmail.com>
2019-11-21 23:41:34 +0600

    add settings button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,19 @@
-import { IonContent, IonPage} from '@ionic/react';
+import { IonContent, IonPage, IonFab, IonFabButton, IonIcon } from '@ionic/react';
 import React from 'react';
 import HolidaysListView from './Holidays/HolidaysListView';
+import settings from 'ionicons/icons/imports/settings';
 
 const Home = () => {
   return (
     <IonPage>
-      <IonContent>
-          <HolidaysListView country="Bangladesh" year={2019} />
+      <IonContent fullscreen>
+       {/*-- fab placed to the top end --*/}
+      <IonFab vertical="bottom" horizontal="end" slot="fixed">
+        <IonFabButton>
+          <IonIcon icon={settings} />
+        </IonFabButton>
+      </IonFab>
+        <HolidaysListView country="Bangladesh" year={2019} />
       </IonContent>
     </IonPage>
   );
