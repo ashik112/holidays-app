abcd6ad455d8ec2e26ef9983ff13546b6e9097a9
Md. Ashekur Rahman <ashekur.rahman@eongroup.net.bd>
2019-11-25 13:12:51 +0600

    demo tableview

---
+++ src/pages/Holidays/HolidaysTableView.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+export default class HolidaysTableView extends Component{
+  render() {
+    return(
+      <>
+        <div>
+          f
+        </div>
+      </>
+    );
+  }
+}
