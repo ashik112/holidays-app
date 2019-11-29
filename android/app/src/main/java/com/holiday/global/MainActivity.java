e6c4bfdbe3d00b23ec47f0f998f75f59650fc5c8
ashik112 <md.ashekur@gmail.com>
2019-11-30 00:17:13 +0600

    android files required: AndroidManifest & MainActivity

---
+++ android/app/src/main/java/com/holiday/global/MainActivity.java
@@ -0,0 +1,25 @@
+package com.holiday.global;
+
+import android.os.Bundle;
+
+import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.Plugin;
+
+import java.util.ArrayList;
+
+// Other imports...
+import app.xplatform.capacitor.plugins.AdMob;
+
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    // Initializes the Bridge
+    this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
+      // Additional plugins you've installed go here
+      // Ex: add(TotallyAwesomePlugin.class);
+       add(AdMob.class);  // Add AdMob as a Capacitor Plugin
+    }});
+  }
+}
