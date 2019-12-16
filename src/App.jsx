da1933e2979f89e93334641d826d5380c360161f
ashik112 <md.ashekur@gmail.com>
2019-12-16 16:40:18 +0600

    enable admob

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
+import React, {Component} from 'react';
+import {Redirect, Route} from 'react-router-dom';
+import {IonApp, IonRouterOutlet} from '@ionic/react';
+import {IonReactRouter} from '@ionic/react-router';
 import Home from './pages/Home';
 
 /* Core CSS required for Ionic components to work properly */
@@ -19,38 +19,39 @@ import '@ionic/react/css/text-alignment.css';
 import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
+import {admobCredentials} from "./constants";
 
 /* Theme variables */
 import './theme/variables.css';
 
-import { Plugins } from '@capacitor/core';
+import {Plugins} from '@capacitor/core';
 
-const { AdMob } = Plugins;
+const {AdMob} = Plugins;
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        try {
-            AdMob.initialize('ca-app-pub-7599602508699161~8716527808');
-        } catch (e) {
-            console.log(e);
-        }
+  constructor(props) {
+    super(props);
+    try {
+      AdMob.initialize(admobCredentials.appID);
+    } catch (e) {
+      console.log(e);
     }
+  }
 
-    render() {
-        return(
-            <IonApp>
-                <IonReactRouter>
-                    <IonRouterOutlet>
-                        <Route path="/home" component={Home} exact={true} />
-                        <Route exact path="/" render={() => <Redirect to="/home" />} />
-                        {/* <Route path="/:tab(sessions)" component={SessionsPage} exact={true} />
+  render() {
+    return (
+      <IonApp>
+        <IonReactRouter>
+          <IonRouterOutlet>
+            <Route path="/home" component={Home} exact={true}/>
+            <Route exact path="/" render={() => <Redirect to="/home"/>}/>
+            {/* <Route path="/:tab(sessions)" component={SessionsPage} exact={true} />
                         <Route path="/:tab(speakers)" component={SpeakerList} exact={true} /> */}
-                    </IonRouterOutlet>
-                </IonReactRouter>
-            </IonApp>
-        );
-    }
+          </IonRouterOutlet>
+        </IonReactRouter>
+      </IonApp>
+    );
+  }
 }
 
 export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ import {
   IonCardTitle,
   IonIcon,
 } from '@ionic/react';
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import HolidaysListView from './Holidays/HolidaysListView';
 //import Settings from '../shared/Settings';
 import storageService from '../services/storeageService';
@@ -16,173 +16,175 @@ import jsonHolidays from '../assets/global_public_holiday';
 import HolidaysTableView from './Holidays/HolidaysTableView';
 import HolidayPageTopBar from './Holidays/components/HolidayPageTopBar';
 import iconFlag from 'ionicons/icons/imports/flag';
-import { Plugins } from '@capacitor/core';
-import { AdSize, AdPosition } from 'capacitor-admob';
+import {Plugins} from '@capacitor/core';
+import {AdSize, AdPosition} from 'capacitor-admob';
 import CountryFab from '../shared/CountryFab';
 import SwitchViewFab from '../shared/SwitchViewFab';
+import {admobCredentials} from "../constants";
 
 // eslint-disable-next-line no-unused-vars
-const { AdMob } = Plugins;
+const {AdMob} = Plugins;
 
 // eslint-disable-next-line no-unused-vars
 const options = {
-	adId: 'ca-app-pub-3940256099942544/6300978111',
-	adSize: AdSize.Banner,
-	position: AdPosition.BOTTOM_CENTER
+  adId: admobCredentials.adID,
+  adSize: AdSize.Banner,
+  position: AdPosition.BOTTOM_CENTER
 };
 
 class Home extends Component {
-	constructor(props) {
-		super(props);
-		this.doneSettings = this.doneSettings.bind(this);
-		this.onClickSettings = this.onClickSettings.bind(this);
-		this.switchView = this.switchView.bind(this);
-		this.contentRef = React.createRef();
-		this.state = {
-			tableView: true,
-			openSettings: false,
-			loaded: false,
-			countryData: {},
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.doneSettings = this.doneSettings.bind(this);
+    this.onClickSettings = this.onClickSettings.bind(this);
+    this.switchView = this.switchView.bind(this);
+    this.contentRef = React.createRef();
+    this.state = {
+      tableView: true,
+      openSettings: false,
+      loaded: false,
+      countryData: {},
+    };
+  }
 
-	componentDidMount() {
-		this.doneSettings().then();
-		try {
-			// Show Banner Ad
-			/*AdMob.showBanner(options)
-				.then(
-					(value) => {
-						console.log(value);  // true
-					},
-					(error) => {
-						console.log(error); // show error
-					}
-				);*/
+  componentDidMount() {
+    this.doneSettings().then();
+    try {
+      // Show Banner Ad
+      AdMob.showBanner(options)
+        .then(
+          (value) => {
+            // console.log(value);  // true
+          },
+          (error) => {
+            // console.log(error); // show error
+          }
+        );
 
-			/*AdMob.addListener('onAdLoaded', (info) => {
-				console.log("Banner Ad Loaded");
-			});*/
-		} catch (e) {
-			console.log(e);
-		}
-	}
+      /*AdMob.addListener('onAdLoaded', (info) => {
+        console.log("Banner Ad Loaded");
+      });*/
+    } catch (e) {
+      console.log(e);
+    }
+  }
 
-	onClickSettings = () => {
-		this.setState({
-			openSettings: true,
-		})
-	};
+  onClickSettings = () => {
+    this.setState({
+      openSettings: true,
+    })
+  };
 
-	cancelSettings = async () => {
-		await this.setState({
-			openSettings: false,
-		});
-	};
+  cancelSettings = async () => {
+    await this.setState({
+      openSettings: false,
+    });
+  };
 
-	doneSettings = async () => {
-		await this.setState({
-			loaded: false,
-		});
-		const country = await storageService.getObject('country');
-		const year = await storageService.getObject('year');
-		if(country===null || year===null) {
-			await this.setState({
-				countryData: {
-					country: null,
-					year: null,
-					days: [],
-				},
-				openSettings: true,
-				loaded: true,
-			});
-		} else {
-			let tempDays = [];
-			if(jsonHolidays && jsonHolidays[country] && jsonHolidays[country][year]) {
-				tempDays = jsonHolidays[country][year];
-			}
-			await this.setState({
-				countryData: {
-					country: country,
-					year: year,
-					days: tempDays,
-				},
-				openSettings: false,
-				loaded: true,
-			});
-		}
-	};
+  doneSettings = async () => {
+    await this.setState({
+      loaded: false,
+    });
+    const country = await storageService.getObject('country');
+    const year = await storageService.getObject('year');
+    if (country === null || year === null) {
+      await this.setState({
+        countryData: {
+          country: null,
+          year: null,
+          days: [],
+        },
+        openSettings: true,
+        loaded: true,
+      });
+    } else {
+      let tempDays = [];
+      if (jsonHolidays && jsonHolidays[country] && jsonHolidays[country][year]) {
+        tempDays = jsonHolidays[country][year];
+      }
+      await this.setState({
+        countryData: {
+          country: country,
+          year: year,
+          days: tempDays,
+        },
+        openSettings: false,
+        loaded: true,
+      });
+    }
+  };
 
-	switchView = () => {
-		const { tableView } = this.state;
-		this.setState({
-			tableView: !tableView,
-		});
-		this.contentRef.current.scrollToTop();
-	};
+  switchView = () => {
+    const {tableView} = this.state;
+    this.setState({
+      tableView: !tableView,
+    });
+    this.contentRef.current.scrollToTop();
+  };
 
-	render() {
-		const { countryData, openSettings, loaded, tableView } = this.state;
-		const { country, year } = countryData;
-		return (
-			<IonPage>
-				<IonContent
-					fullscreen
-					ref={this.contentRef}
-					scrollEvents={true}
-				>
-					{
-						loaded && (
-							<>
-								<CountryFab
-									country={country}
-									year={year}
-									onOpen={this.onClickSettings}
-									onDone={this.doneSettings}
-									onCancel={this.cancelSettings}
-									open={(openSettings)}
-								/>
-								<SwitchViewFab
-									switchView={this.switchView}
-								/>
-								<div
-									style={{
-										display: 'block',
-										position: "fixed",
-										top: 0,
-										width: '100%',
-										zIndex: 1,
-									}}
-								>
-									<HolidayPageTopBar
-										countryName={country===null? 'UNKNOWN' : country}
-										year={`${year===null? 'UNKNOWN' : year}`}
-									/>
-								</div>
-								<div style={{
-									position: "relative",
-									marginTop: '97px',
-								}}>
-									{(country) && (year) && tableView && (<HolidaysTableView style={{ position: 'relative' }} countryData={countryData} />)}
-									{(country) && (year) && !tableView && (<HolidaysListView countryData={countryData} />)}
-									{((!country) || (!year)) && (
-									    <IonCard>
-                                          <IonCardHeader>
-                                            <IonCardTitle>No Data!</IonCardTitle>
-                                          </IonCardHeader>'
-                                          <IonCardContent>
-                                            Please select country and year. Press <IonIcon icon={iconFlag} />
-                                          </IonCardContent>
-                                        </IonCard>
-                                    )}
-								</div>
-							</>
-						)
-					}
-				</IonContent>
-			</IonPage>
-		);
-	}
+  render() {
+    const {countryData, openSettings, loaded, tableView} = this.state;
+    const {country, year} = countryData;
+    return (
+      <IonPage>
+        <IonContent
+          fullscreen
+          ref={this.contentRef}
+          scrollEvents={true}
+        >
+          {
+            loaded && (
+              <>
+                <CountryFab
+                  country={country}
+                  year={year}
+                  onOpen={this.onClickSettings}
+                  onDone={this.doneSettings}
+                  onCancel={this.cancelSettings}
+                  open={(openSettings)}
+                />
+                <SwitchViewFab
+                  switchView={this.switchView}
+                />
+                <div
+                  style={{
+                    display: 'block',
+                    position: "fixed",
+                    top: 0,
+                    width: '100%',
+                    zIndex: 1,
+                  }}
+                >
+                  <HolidayPageTopBar
+                    countryName={country === null ? 'UNKNOWN' : country}
+                    year={`${year === null ? 'UNKNOWN' : year}`}
+                  />
+                </div>
+                <div style={{
+                  position: "relative",
+                  marginTop: '97px',
+                }}>
+                  {(country) && (year) && tableView && (
+                    <HolidaysTableView style={{position: 'relative'}} countryData={countryData}/>)}
+                  {(country) && (year) && !tableView && (<HolidaysListView countryData={countryData}/>)}
+                  {((!country) || (!year)) && (
+                    <IonCard>
+                      <IonCardHeader>
+                        <IonCardTitle>No Data!</IonCardTitle>
+                      </IonCardHeader>'
+                      <IonCardContent>
+                        Please select country and year. Press <IonIcon icon={iconFlag}/>
+                      </IonCardContent>
+                    </IonCard>
+                  )}
+                </div>
+              </>
+            )
+          }
+        </IonContent>
+      </IonPage>
+    );
+  }
 }
 
 export default withIonLifeCycle(Home);
